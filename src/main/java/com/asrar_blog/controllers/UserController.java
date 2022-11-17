e30622d6e45bc66e1e6618dbc1e3c0364f6ad3db
Syed Asrar Zahoor <81914950+Syed-Asrar@users.noreply.github.com>
2022-11-17 11:58:48 +0530

    UserNotFoundException handler added

--- src/main/java/com/asrar_blog/controllers/UserController.java
+++ src/main/java/com/asrar_blog/controllers/UserController.java
@@ -1,6 +1,6 @@
 package com.asrar_blog.controllers;
 
-import com.asrar_blog.entities.User;
+import com.asrar_blog.payloads.ApiResponse;
 import com.asrar_blog.payloads.UserDTO;
 import com.asrar_blog.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,28 +11,34 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/user")
 public class UserController {
     @Autowired
     UserService userService;
 
-    @GetMapping("/users")
+    @GetMapping("/all")
     public ResponseEntity<List<UserDTO>> getUsers(){
         List list = userService.getAllUsers();
         return new ResponseEntity<>(list, HttpStatus.OK);
     }
-    @PostMapping("/user")
+    @PostMapping("/")
     public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO user){
         UserDTO userDto = userService.createUser(user);
         return new ResponseEntity<>(userDto, HttpStatus.CREATED);
     }
-    @GetMapping("/user/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<UserDTO> getUser(@PathVariable("id") int id){
         UserDTO user = userService.getGetUserById(id);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
-    @DeleteMapping("/user/{id}")
-    public ResponseEntity<String> deleteUser(@PathVariable("id") int id){
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDTO> updateUser(@RequestBody UserDTO userDTO, @PathVariable int id){
+        UserDTO user = userService.updateUser(userDTO,id);
+        return new ResponseEntity<>(user,HttpStatus.OK);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse> deleteUser(@PathVariable("id") int id){
         userService.deleteUser(id);
-        return new ResponseEntity<>("Success",HttpStatus.OK);
+        return new ResponseEntity<>(new ApiResponse("Deleted sucessfully",true),HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/asrar_blog/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.asrar_blog.exceptions;
+
+import com.asrar_blog.payloads.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse> resourceNotFoundExceptionHandler(ResourceNotFoundException ex){
+        String message = ex.getMessage();
+        return new ResponseEntity<>(new ApiResponse(message,false), HttpStatus.NOT_FOUND);
+    }
+}
